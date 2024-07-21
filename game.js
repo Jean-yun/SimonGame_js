f1cebc2701ba2d643aa9b67ee34fa354f277e1ed
Jean-yun <yesirchloe@gmail.com>
2024-07-22 00:15:55 +0900

    modified game process (works successfully) and when wrong answer

--- game.js
+++ game.js
@@ -1,14 +1,30 @@
 var gamePattern = []
 var buttonColors = ["red","blue","green","yellow"]
 var userClickedPattern = []
+var started = false
 var level = 0
+
+
+// when key pressed, the game Start
+$(document).keydown(function() {
+	//if not started yet, text changes 
+	if (!started) {
+		$("#level-title").text("Level " + level)
+		nextSequence()
+		started = true;
+	} 
+})
+
 
 //Creating new problem set 
 function nextSequence() {
+	//* user need to click from the beginning, so reset the userClickedPattern
+	userClickedPattern = []
+
 	//1. add random color to the List  (Game Stage)
 	var randomNumber = Math.floor(Math.random() * 4);
 	var randomChosenColor = buttonColors[randomNumber];
-	gamePattern.push(randomChosenColor)
+	gamePattern.push(randomChosenColor)	
 
 	//2. add Audio sounds when added 
 	playSound(randomChosenColor)
@@ -16,13 +32,12 @@ function nextSequence() {
 	//3. add animation to the certain button
 	$("."+randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100)
 
-	//4. Increase Level each time the function is called
+	//4. Increase Level each time the function is called, shows in H1
 	level ++ 
+	$("#level-title").text("Level " + level);
 
-	//5. Undate h1 with the changed value of level
-	$("h1").html("Level " + level)
 	//Check the color
-	console.log(randomChosenColor)
+	console.log(gamePattern)
 }
 
 
@@ -38,9 +53,13 @@ $(".btn").on("click", function(event) {
 	//3. Animate button when pressed
 	animatePress(userChosenColor)
 
+	//4. check the answer
+	checkAnswer(userClickedPattern.length - 1)
+
 	//Check the color
 	console.log(userClickedPattern)
 })
+
 
 
 
@@ -58,10 +77,20 @@ function animatePress(currentColor) {
 }
 
 
-// when key pressed, the game Start
-$(document).keydown(function() {
-	nextSequence()
-	$("#level-title").text("Level " + level)
-
-})
-
+function checkAnswer(currentLevel) {
+	if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+	console.log("success")
+		if (userClickedPattern.length === gamePattern.length) {
+			setTimeout(function() {nextSequence()}, 1000)
+			}}
+	else {
+	//when wrong, wrong sound & background & go back to the first screen
+	console.log("wrong")
+	$("body").addClass("game-over")
+	setTimeout(function() {$("body").removeClass("game-over")}, 200)
+	playSound("wrong")
+	$("#level-title").text("Game Over, Press Any Key to Restart")
+	started = false
+	level = 0
+	}
+}
