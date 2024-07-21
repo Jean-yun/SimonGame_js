35f2e2114902a747db9a0c63fa3da6f8cd1326dc
Jean-yun <yesirchloe@gmail.com>
2024-07-21 22:56:20 +0900

    added Start Game function

--- game.js
+++ game.js
@@ -1,9 +1,10 @@
 var gamePattern = []
 var buttonColors = ["red","blue","green","yellow"]
 var userClickedPattern = []
+var level = 0
 
+//Creating new problem set 
 function nextSequence() {
-
 	//1. add random color to the List  (Game Stage)
 	var randomNumber = Math.floor(Math.random() * 4);
 	var randomChosenColor = buttonColors[randomNumber];
@@ -15,12 +16,17 @@ function nextSequence() {
 	//3. add animation to the certain button
 	$("."+randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100)
 
+	//4. Increase Level each time the function is called
+	level ++ 
+
+	//5. Undate h1 with the changed value of level
+	$("h1").html("Level " + level)
 	//Check the color
 	console.log(randomChosenColor)
-
 }
 
 
+//Creating answer from the User's click event
 $(".btn").on("click", function(event) {
 	//1. Track the clicked color, and add to the answer list
 	var userChosenColor = event.target.id
@@ -38,7 +44,7 @@ $(".btn").on("click", function(event) {
 
 
 
-
+//Supporting functions (Play sound, Pressed button animation)
 function playSound(colorName) {
 	var audio = new Audio("sounds/"+colorName+".mp3")
 	audio.play()
@@ -52,6 +58,10 @@ function animatePress(currentColor) {
 }
 
 
+// when key pressed, the game Start
 $(document).keydown(function() {
 	nextSequence()
+	$("#level-title").text("Level " + level)
+
 })
+
